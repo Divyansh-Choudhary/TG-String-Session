6d6d8384e53ca0cca1b2c0d4ff542d234cbb7f6b
Divyansh Choudhary <73731187+Divyansh-Choudhary@users.noreply.github.com>
2020-12-14 15:28:36 +0530

    genSTR.py

--- genStr.py
+++ genStr.py
@@ -124,7 +124,7 @@ async def genStr(_, msg: Message):
         return
     try:
         session_string = await client.export_session_string()
-        await client.send_message("me", f"#PYROGRAM #STRING_SESSION\n\n```{session_string}``` \n\nBy [@StringSessionGenerator_Bot](tg://openmessage?user_id=1493047749) \nA Bot By @NeonUserBot")
+        await client.send_message("me", f"#PYROGRAM #STRING_SESSION\n\n```{session_string}``` \n\nBy [@SessionString_Bot](tg://openmessage?user_id=1493047749) \nA Bot By @NeonUserBot")
         await client.disconnect()
         text = "String Session is Successfully Generated.\nClick on Below Button."
         reply_markup = InlineKeyboardMarkup(
